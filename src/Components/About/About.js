8467e20791df41bfba7114ff34c23af5f581a3d4
Nisharga Kabir <34031225+nisharga@users.noreply.github.com>
2022-06-22 23:04:21 +0600

    about us page done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,20 +1,34 @@
 import React from "react";
+import photo from "./blank_profile.jpg";
+import "./About.css";
+import LeafletMap from "./leafletMap/LeafletMap";
 
 const About = () => {
   return (
     <div className="about bg-light pt-4 pb-4">
       <h2 className="text-center mb-4">About Me</h2>
       <div className="d-flex justify-content-center">
-        <div className="card text-center">
+        <div className="card text-center p-4">
           <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
+            <img src={photo} alt={"blank img"} />
+            <h5 className="card-title mt-3">Nisharga Kabir</h5>
             <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
+              <b>My Goal :</b> First goal definitely a weakly base target set.
+              But thats not mean that think just 10 minutes after then sleep.
+              Work hard 2/3 days, after that panicked then again set a new goal.
+              The tiger has only one target. Deer hunting. Same happened to me
+              just set a 1 goal untill it finish. Everyday managing 5 hours to
+              achieve my goal. If faced any problem always find solution. If
+              can't find then skip it and try new one. Try to learn it Later. If
+              family matter or something personal issue happened then after a
+              small break give my 120% to quickly find the track. Give 6/7 hours
+              to finding the right path.{" "}
             </p>
           </div>
         </div>
       </div>
+
+      <LeafletMap></LeafletMap>
     </div>
   );
 };
---
+++ src/Components/About/leafletMap/LeafletMap.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+const LeafletMap = () => {
+  const position = [23.7937, 90.4054];
+  return (
+    <div className="map">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default LeafletMap;
