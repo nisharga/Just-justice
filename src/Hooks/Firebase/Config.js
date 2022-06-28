fc48120c07809d7739eb3c44d49cc89bf35d8ee1
Nisharga Kabir <34031225+nisharga@users.noreply.github.com>
2022-06-28 22:21:17 +0600

    bug fix

--- src/Hooks/Firebase/Config.js
+++ src/Hooks/Firebase/Config.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBKIZXEPmbtEko62AQpUFrGAhVBeqg5Pqs",
   authDomain: "just-justice-1.firebaseapp.com",
-  projectId: "just-justice-1.firebaseapp.com",
+  projectId: "just-justice-1",
   storageBucket: "just-justice-1.appspot.com",
   messagingSenderId: "1082736808966",
   appId: "1:1082736808966:web:7a93cf3c25b5d32636df87",
